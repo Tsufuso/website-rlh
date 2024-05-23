b41b78eba3bb0482f481bfd5c68ea93bc965e907
RLH <aminehajji805@gmail.com>
2024-05-23 03:55:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const sidenav = document.querySelector('.sidenav');
+    const uirBtn = document.getElementById('uir-btn');
+    const uirSubmenu = document.getElementById('uir-submenu');
+  
+    // Handle links for moving the profile image
     const links = document.querySelectorAll('.sidenav a');
     const profileImage = document.querySelector('.profile-image');
-    const sidenav = document.querySelector('.sidenav');
-
+  
     links.forEach(link => {
-        link.addEventListener('click', function () {
-            // Vérifiez si le lien cliqué est différent de l'accueil
-            if (link.getAttribute('href') !== 'index.html') {
-                // Déplacez la photo de profil au-dessus de la barre latérale
-                sidenav.insertBefore(profileImage, sidenav.firstChild);
-            } else {
-                // Remettre la photo de profil à son emplacement initial
-                sidenav.appendChild(profileImage);
-            }
-        });
+      link.addEventListener('click', function () {
+        if (link.getAttribute('href') !== 'index.html') {
+          sidenav.insertBefore(profileImage, sidenav.firstChild);
+        } else {
+          sidenav.appendChild(profileImage);
+        }
+      });
     });
-});
+
+    // Toggle the display of the submenu when clicking on the UIR button
+    uirBtn.addEventListener('click', function() {
+        if (uirSubmenu.style.display === 'block') {
+          uirSubmenu.style.display = 'none'; // Hide the submenu if it's already visible
+        } else {
+          uirSubmenu.style.display = 'block'; // Show the submenu if it's hidden
+        }
+      });
+  });
